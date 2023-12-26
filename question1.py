f53be9c2bc9c787c0772b1187800f9a8335ef2d2
Lubna Intezar <lubna.khan2609@gmail.com>
2023-12-26 17:52:20 +0000

    add first file

---
+++ question1.py
@@ -0,0 +1,13 @@
+#.........codefirstgirl.............
+#.........Session1..................
+#.........question1.................
+
+
+num_chair='15'  # string can't multiply by integer
+num_nails=4
+total_nails=int(num_chair)*num_nails
+message="I need to buy {} nails".format(total_nails)
+print(message)
+
+#output display:
+#I need to buy 60 nails
