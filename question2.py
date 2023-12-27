f373af16ffad9a554380c27fb5be55bd486a85d8
Lubna Intezar <lubna.khan2609@gmail.com>
2023-12-27 00:23:10 +0000

    Adding  second file

---
+++ question2.py
@@ -0,0 +1,11 @@
+#..........codeFirstGirl................
+# ..........question 2..................
+#......fixing the error.................
+#my_name=Penelope
+
+my_name="Penelope"
+my_age=29
+message="My name is {} and I am {} year's old".format(my_name,my_age)
+print(message)
+
+#my_name should be define as a string by putting " " quotes
